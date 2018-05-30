ffb641f5e588cce1d0e9162932c7bccc80487af4
Scordavis <scordavis@gmail.com>
2018-05-30 22:28:43 +0300

    full code desktop
    Десктопная версия, без настройки форм

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,44 @@
-$(function() {
+// $(function() {
 
 	// Custom JS
+
+// insert slider img src to background-image
+
+
+
+// var arr = $('.gallery_item');
+// arr.each(function( i, el) {
+// var bgSrc = $(this).find('.js-image').attr('src');
+//  $(this).find('.js-background').css('background-image', 'url("' + bgSrc + '")');
+// });
+
+// $(this).find(".gallery__item")(function(){
+// 	var bgSrc = $('.gallery__item > .js-background > .js-image').attr('src');
+// 	 $('.gallery__item > .js-background').css('background-image', 'url("' + bgSrc + '")');
+// });
+
+
+
+// var bgSrc = $('.js-image').attr('src');
+// $('.js-background').css('background-image', 'url("' + bgSrc + '")');
+
+
+
+
+// });
+
+$( document ).ready(function() {
+
+// slick
+$('.gallery__wrapper').slick({
+	dots: true,
+	arrows: true
+});
+
+$(".js-image").each(function(){
+  var obj = $(this).attr("src");
+  $(this).wrap("<div class='gallery__img-wrapper js-background' style='background:url(" + obj + "); '></div>")
+})
+
 
 });
